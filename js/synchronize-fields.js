aa36afe33908a0ae660a779fd2bd478c6d8c1242
Kate <petryaeva.e@gmail.com>
2017-02-14 18:00:51 +0300

    разбила form.js на модули

---
+++ js/synchronize-fields.js
@@ -0,0 +1,14 @@
+'use strict';
+
+window.synchronizeFields = function (elem1, elem2, arrea1, arrea2, nameProp) {
+
+  elem1.addEventListener('change', function () {
+    var value = arrea1.indexOf(elem1.value);
+    elem2[nameProp] = arrea2[value];
+  });
+
+  elem2.addEventListener('change', function () {
+    var value = arrea2.indexOf(elem2.value);
+    elem1[nameProp] = arrea1[value];
+  });
+};
