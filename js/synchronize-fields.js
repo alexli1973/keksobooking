c8eb6a7202b90c90b015b3cf3ef610c87f179a6a
Kate <petryaeva.e@gmail.com>
2017-02-15 09:25:42 +0300

    исправила synchronizeFields

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -8,7 +8,7 @@ window.synchronizeFields = function (elem1, elem2, arrea1, arrea2, nameProp) {
   });
 
   elem2.addEventListener('change', function () {
-    var value = arrea2.indexOf(elem2.value);
+    var value = arrea2[elem2.value];
     elem1[nameProp] = arrea1[value];
   });
 };
