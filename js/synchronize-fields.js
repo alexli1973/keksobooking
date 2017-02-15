beda195199b00c402d14334561738f7c68f34f18
Kate <petryaeva.e@gmail.com>
2017-02-15 09:46:37 +0300

    исправила synchronizeFields

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,12 +3,10 @@
 window.synchronizeFields = function (elem1, elem2, arrea1, arrea2, nameProp) {
 
   elem1.addEventListener('change', function () {
-    var value = arrea1[elem1.value];
-    elem2[nameProp] = arrea2[value];
+    elem2[nameProp] = arrea2[nameProp];
   });
 
   elem2.addEventListener('change', function () {
-    var value = arrea2[elem2.value];
-    elem1[nameProp] = arrea1[value];
+    elem1[nameProp] = arrea1[nameProp];
   });
 };
