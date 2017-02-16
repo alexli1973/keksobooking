648d8cb1e6614ebbb3ef00da59d89b0ccffa90c5
Kate <petryaeva.e@gmail.com>
2017-02-16 10:45:49 +0300

    вынесла обработчики событий в toggle-pin

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,7 +1,6 @@
 'use strict';
 
-window.synchronizeFields = function (elem1, elem2, arrea1, arrea2, nameProp) {
-
+window.synchronizeFields = (function (elem1, elem2, arrea1, arrea2, nameProp) {
   elem1.addEventListener('change', function () {
     var value = arrea1.indexOf(elem1.value);
     elem2[nameProp] = arrea2[value];
@@ -11,4 +10,4 @@ window.synchronizeFields = function (elem1, elem2, arrea1, arrea2, nameProp) {
     var value = arrea2.indexOf(elem1.value);
     elem1[nameProp] = arrea1[value];
   });
-};
+})();
