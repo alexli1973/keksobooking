92e745f6cc2afe6249db9ad86dcec82616421440
Kate <petryaeva.e@gmail.com>
2017-02-19 17:05:23 +0300

    добавила коллбэк в ф-ю synchronizeFields

--- js/form.js
+++ js/form.js
@@ -7,7 +7,9 @@
   var time = document.querySelector('#time');
   var timeout = document.querySelector('#timeout');
 
-  window.synchronizeFields(time, timeout, ['12', '13', '14'], ['12', '13', '14'], 'value');
+  window.synchronizeFields(time, timeout, ['12', '13', '14'], ['12', '13', '14'], function (value) {
+    timeout.value = value;
+  });
 
   // 2. Значение поля «Тип жилья» синхронизировано с минимальной ценой следующим образом:
   // «Квартира» — минимальная цена 1000
@@ -17,11 +19,15 @@
   var capacity = document.querySelector('#capacity');
   var price = document.querySelector('#price');
 
-  window.synchronizeFields(typeHouse, price, ['flat', 'shack', 'palace'], ['1000', '1000', '10000'], 'value');
+  window.synchronizeFields(typeHouse, price, ['flat', 'shack', 'palace'], ['1000', '1000', '10000'], function (value) {
+    price.value = value;
+  });
 
   // 3. Количество комнат связано с количеством гостей:
   // 2 или 100 комнат — «для 3 гостей»; 1 комната — «не для гостей»
   var roomNumber = document.querySelector('#room_number');
 
-  window.synchronizeFields(roomNumber, capacity, ['1', '2', '100'], ['0', '0', '3'], 'value');
+  window.synchronizeFields(roomNumber, capacity, ['1', '2', '100'], ['0', '0', '3'], function (value) {
+    capacity.value = value;
+  });
 })();
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,15 +1,15 @@
 'use strict';
 
 window.synchronizeFields = (function () {
-  return function (elem1, elem2, arrea1, arrea2, nameProp) {
+  return function (elem1, elem2, arrea1, arrea2, syncValues) {
     elem1.addEventListener('change', function () {
       var value = arrea1.indexOf(elem1.value);
-      elem2[nameProp] = arrea2[value];
+      syncValues(arrea2[value]);
     });
 
     elem2.addEventListener('change', function () {
       var value = arrea2.indexOf(elem1.value);
-      elem1[nameProp] = arrea1[value];
+      syncValues(arrea1[value]);
     });
   };
 })();
