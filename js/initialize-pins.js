168cd72576d880d48d2447ef27ee36ef442edd5c
Kate <petryaeva.e@gmail.com>
2017-02-20 14:41:33 +0300

    вынесла открытие диалога в show-card

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -2,40 +2,12 @@
 
 window.initializePins = (function () {
   var pins = document.querySelectorAll('.pin');
-  var dialog = document.querySelector('.dialog');
-  var onDialogClose = null;
-
-  function dialogKeydownHendler(evt) {
-    if (window.utils.isDeactivateEvent(evt)) {
-      closeDialogClickHendler();
-    }
-  }
 
   // деативировать все метки при закрытии .dialog
   function closeDialogClickHendler() {
-    closeDialog();
+    window.showCard.closeDialog();
     disablePin();
   }
-
-  // открыть окно с объявлением по клику на .pin
-  function openDialog() {
-    dialog.style.display = 'block';
-    document.addEventListener('keydown', dialogKeydownHendler);
-  }
-
-  // закрыть окно с объявлением по клику на .pin
-  function closeDialog() {
-    dialog.style.display = 'none';
-    document.removeEventListener('keydown', dialogKeydownHendler);
-
-    if (typeof onDialogClose === 'function') {
-      onDialogClose();
-    }
-  }
-
-  // окно с объявлением изначально скрыто
-  // все метки изначально неактивны
-  closeDialogClickHendler();
 
   // сделать метку неактивной
   function disablePin() {
@@ -64,37 +36,32 @@ window.initializePins = (function () {
     }
     disablePin();
     activePin(targetPin);
-    openDialog();
+    window.showCard.openDialog();
     if (window.utils.isActivateEvent(evt)) {
       var curentPin = event.target;
       disablePin();
       activePin(curentPin);
-      openDialog();
+      window.showCard.openDialog();
     }
   }
 
   // вернуть фокус на активную метку,
   // если карточка была открыта с клавиатуры
-  function focusOpenButton(evt) {
-    var elem = evt.target.classList.contains('pin');
-    return elem.focus();
+  function focusPin() {
+    var pinActive = document.querySelector('.pin--active');
+    pinActive.focus();
   }
 
   function pinKeydownHandler(evt) {
     if (window.utils.isActivateEvent(evt)) {
       pinClickHandler(evt);
-      focusOpenButton();
+      window.showCard(focusPin);
     }
-  }
-
-  function cbCloseDialog(cb) {
-    onDialogClose = cb;
   }
 
   return {
     pinClickHandler: pinClickHandler,
     pinKeydownHandler: pinKeydownHandler,
-    closeDialogClickHendler: closeDialogClickHendler,
-    cbCloseDialog: cbCloseDialog
+    closeDialogClickHendler: closeDialogClickHendler
   };
 })();
--- js/show-card.js
+++ js/show-card.js
@@ -1,5 +1,36 @@
 'use strict';
 
 window.showCard = (function () {
+  var dialog = document.querySelector('.dialog');
+  var onDialogClose = null;
 
+  function dialogKeydownHendler(evt) {
+    if (window.utils.isDeactivateEvent(evt)) {
+      window.initializePins.closeDialogClickHendler();
+    }
+  }
+
+  // открыть окно с объявлением по клику на .pin
+  function openDialog() {
+    dialog.style.display = 'block';
+    document.addEventListener('keydown', dialogKeydownHendler);
+  }
+
+  // закрыть окно с объявлением по клику на .pin
+  function closeDialog() {
+    dialog.style.display = 'none';
+    document.removeEventListener('keydown', dialogKeydownHendler);
+
+    if (typeof onDialogClose === 'function') {
+      onDialogClose();
+    }
+  }
+
+  // окно с объявлением изначально скрыто
+  // все метки изначально неактивны
+  window.initializePins.closeDialogClickHendler();
+
+  return function (cb) {
+    onDialogClose = cb;
+  };
 })();
