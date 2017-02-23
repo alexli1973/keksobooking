d720bbbad1513324f826dc855265493c6d0d6485
Kate <petryaeva.e@gmail.com>
2017-02-24 00:18:36 +0300

    добавила коллюэк с фокусом на текущую метку

--- js/show-card.js
+++ js/show-card.js
@@ -1,16 +1,7 @@
 'use strict';
 
 window.showCard = (function () {
-  var onDialogClose;
-  var dialog = document.querySelector('.dialog');
-
-  return function (cb) {
-    dialog.style.display = 'block';
-
-    if (typeof onDialogClose === 'function') {
-      onDialogClose();
-    }
-
-    onDialogClose = cb;
+  return function (element) {
+    element.style.display = 'block';
   };
 })();
